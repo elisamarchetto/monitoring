003c6522c9f6d9486f73519ce0cc57a1c189d8d0
elisamarchetto <62661809+elisamarchetto@users.noreply.github.com>
2020-03-27 18:08:43 +0100

    Create R_code_multipanel.r

---
+++ R_code_multipanel.r
@@ -0,0 +1,23 @@
+#multipanel in R: monitoring ecosystems
+
+install.packages("GGally")
+library(sp)
+#require(sp) is the same of library(sp)
+data(meuse)
+attach(meuse) #make use of data set of meuse
+
+meuse #to see all the variables
+plot(cadmium)
+
+#make che pairs vairables in plot
+pairs(meuse) #intersections of variables in a kind of matrix
+
+#to correlate only some variables in the plot but we need to indicate the data set that we are going to use
+pairs(~ cadmium+copper+lead+zinc,data=meuse)
+pairs(meuse[,3:6])
+#16 and 15 are the same function!
+
+pairs(meuse[,3:6],pch=19)
+
+library(GGally)
+ggpairs(meuse[,3:6])
