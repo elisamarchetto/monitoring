051fe0bb20dc278a8f1439e4a25eee0a9ed8b960
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2020-05-20 17:38:07 +0200

    Update R_code_faPAR.r

--- R_code_faPAR.r
+++ R_code_faPAR.r
@@ -29,3 +29,37 @@ writeRaster(copNDVI, "copNDVI.tif") #write the data copNDVI in .tif, 5.5MB
 faPAR <- stretch(faPAR10,minv=0,maxv=250)
 writeRaster(faPAR, "faPAR.tif")
 faPAR
+
+### regression model between faPAR and NDVI
+
+#exemple:
+erosion <- c(12, 14, 16, 24, 26, 40, 55, 67)
+hm <- c(30, 100, 150, 200, 260, 340, 460, 600)
+plot(erosion, hm, col="red", pch=19, xlab="erosion", ylab="heavy metals")
+model1 <- lm(hm ~ erosion) # hm is y ax, and erosion x
+summary(model1)
+# hm= 9.2752erosion-26.9888, R-squared:  0.9747, random situation??  p-value: 5.127e-06
+# line described by slope b and intersection a: abline function
+abline(model1)
+faPAR10 <- raster("faPAR10.tif") #library(raster)
+#library(rasterdiv)
+copNDVI <- reclassify(copNDVI, cbind(253:255, NA), right=TRUE)
+
+install.packages("sf")
+library(sf) # to call st_* functions
+random.points <- function(x,n)
+{
+lin <- rasterToContour(is.na(x))
+pol <- as(st_union(st_polygonize(st_as_sf(lin))), 'Spatial') # st_union to dissolve geometries
+pts <- spsample(pol[1,], n, type = 'random')
+}
+pts <- random.points(faPAR10,1000) # in this case we use 1000 points in stead of all the values in faPAR10
+
+copNDVIp <- extract(copNDVI, pts) # correlate the values of copNDVI with each random points choosen
+faPAR10p <- extract(faPAR10,pts)
+#build the linear model between copNDVIp and faPAR10 (copNDVIp because the calculation is faster with less values)
+# the line is calculated by reducing the distance between the points (x;y) in the graph
+# phothosythesis vs biomass
+model2 <- lm(faPAR10p ~ copNDVIp) # R = 04 because in conifer forest biomass is high and phothosynthesis but not that high. p = 2 ^-16 two variables are related each other
+plot(faPAR10p, copNDVIp)
+abline(model2, col="red")
