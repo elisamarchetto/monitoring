9ca8dbf90dbbdb6e8273bd9dbfdf4168e2b37222
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2020-05-27 18:00:22 +0200

    Update R_code_radiance.r

--- R_code_radiance.r
+++ R_code_radiance.r
@@ -1,12 +1,12 @@
 #R_code_radiance.r
 
 library(raster)
-toy <- raster(ncol=2, nrow=2, xmn=1, xmx=2, ymn=1, ymx=2)
+toy <- raster(ncol=2, nrow=2, xmn=1, xmx=2, ymn=1, ymx=2) # for creating the matrix as a layer
 values(toy) <- c(1.13,1.44,1.55,3.4)
 plot(toy)
 text(toy, digits=2)
-toy2bits <- stretch(toy,minv=0,maxv=3)
-storage.mode(toy2bits[]) = "integer"
+toy2bits <- stretch(toy,minv=0,maxv=3) # reshape the matrix with 2^2 pixel (2 bits), so 4 values
+storage.mode(toy2bits[]) = "integer" # integer values
 plot(toy2bits) #bits: possible combinations of pixels based on NDs
 text(toy2bits, digits=2) #lower is the amount of bits lower is the diversity
 toy4bits <- stretch(toy,minv=0,maxv=15)
