065832c9a14b0d7fe502edc0d5c9bd2c78d6f261
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2020-05-28 19:28:44 +0200

    Update R_code_multipanel.r

--- R_code_multipanel.r
+++ R_code_multipanel.r
@@ -1,6 +1,6 @@
 #multipanel in R: monitoring ecosystems
 
-install.packages("GGally")
+install.packages("GGally") #'GGally' extends 'ggplot2' by adding several functions to reduce the complexity of combining geometric objects
 library(sp)
 #require(sp) is the same of library(sp)
 data(meuse)
@@ -10,11 +10,12 @@ meuse #to see all the variables
 plot(cadmium)
 
 #make che pairs vairables in plot
-pairs(meuse) #intersections of variables in a kind of matrix
+pairs(meuse) #intersections of variables in a kind of matrix. pairs is a function stored in GGally (in this case)
 
-#to correlate only some variables in the plot but we need to indicate the data set that we are going to use
+#to correlate only some variables in the plot. It needs to be indicated the dataset that is going to be used
 pairs(~ cadmium+copper+lead+zinc,data=meuse)
-pairs(meuse[,3:6]) #from 3(cadmium) to 6(zinc)
+#or
+pairs(meuse[,3:6]) # [,3:6] from 3(cadmium) to 6(zinc)
 #16 and 15 are the same function!
 
 pairs(meuse[,3:6],pch=19)
