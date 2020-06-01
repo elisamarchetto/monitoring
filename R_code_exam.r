d2450770077d59e4b18b09c8d7e454096a7a844e
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2020-06-01 16:28:23 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -6,10 +6,22 @@ library(raster)
 library(rasterVis)
 library(RStoolbox)
 
-setwd("C/lab/")
+setwd("C/lab/exam/")
 
-gdal_translate("T33XVJ_20170803T125711_B01.jp2", "B02.tif")
-gdal_translate("T33XVJ_20170803T125711_B01.jp2", "B03.tif")
-gdal_translate("T33XVJ_20170803T125711_B01.jp2", "B04.tif")
-gdal_translate("T33XVJ_20170803T125711_B01.jp2", "B08.tif")
+# Sentinel-2 satellite data
+
+gdal_translate("T30SWG_20191016T110041_B02.jp2", "B02.tif")
+gdal_translate("T30SWG_20191016T110041_B03.jp2", "B03.tif")
+gdal_translate("T30SWG_20191016T110041_B04.jp2", "B04.tif")
+gdal_translate("T30SWG_20191016T110041_B08.jp2", "B08.tif")
+
+octB02 <- raster("B02.tif")
+octB03 <- raster("B03.tif")
+octB04 <- raster("B04.tif")
+octB08 <- raster("B08.tif")
+
+#or directly in SNAP
+
+
+
 
