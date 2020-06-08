122192bd47e81f427359775914787716a0f54737
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2020-06-08 18:08:13 +0200

    Update R_code_snow.r

--- R_code_snow.r
+++ R_code_snow.r
@@ -31,6 +31,23 @@ snow.multitemp <- stack(import)# MULTITEMPORAL ANALYSISSSSSS, import the RasterS
 plot(snow.multitemp, col=cl)
 
 # let's make a prediction
+## prediction
+
+#require(raster)
+#require(rgdal)
+
+# define the extent
+#ext <- c(-180, 180, -90, 90)
+#extension <- crop(snow.multitemp, ext)
+    
+# make a time variable (to be used in regression)
+#time <- 1:nlayers(snow.multitemp)
+
+# run the regression
+#fun <- function(x) {if (is.na(x[1])){ NA } else {lm(x ~ time)$coefficients[2] }} 
+#predicted.snow.2025 <- calc(extension, fun) # time consuming: make a pause!
+#predicted.snow.2025.norm <- predicted.snow.2025*255/53.90828
+
 source("prediction.r") #read r code
 
 #setwd("C:/lab/snow/") and library(raster)
