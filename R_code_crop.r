ba4859ad02cfb4c1003e2b49b8db58e93d5adf60
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2020-06-08 16:56:46 +0200

    Create R_code_crop.r

---
+++ R_code_crop.r
@@ -0,0 +1,9 @@
+setwd("C:/lab/")
+library(ncdf4)
+library(raster)
+
+snow <- raster("c_gls_SCE_202005260000_NHEMI_VIIRS_V1.0.1.nc")
+ext <- c(0, 20, 35, 50)
+zoom(snow, ext=ext)
+snowitaly <- crop(snow, ext)
+zoom(snow, ext=drawExtent())
