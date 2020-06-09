61595856d07a66a951f4172fa14fbb6049fcf47b
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2020-06-09 16:31:29 +0200

    Update R_code_no2.r

--- R_code_no2.r
+++ R_code_no2.r
@@ -11,7 +11,7 @@ plot(EN, col=cl)
 par(mfrow=c(1,2)) # to see the change through time
 plot(EN$EN_0001, col=cl)
 plot(EN$EN_0013, col=cl)
-# 3 layers RGB image from EN to see difference in time in the same graph
+# 3 layers RGB image from EN. See where and when Europe was more affected by NO2 pollution
 plotRGB(EN, r=1, g=7, b=13, stretch="lin")
 #difference map between 2 situations
 dif <- EN$EN_0013 - EN$EN_0001
@@ -19,7 +19,7 @@ cld <- colorRampPalette(c('blue','white','red'))(100) #
 plot(dif, col=cld) 
 
 # Quantitative decrease of no2 
-boxplot(EN) # ELISA JKFGJSGBJGJKFGBJKFDBJKFJK
+boxplot(EN) # five-number summary is the minimum, first quartile, median, third quartile, and maximum. The first quartile is the median of the data points to the left of the median.
 boxplot(EN,outline=F)
 boxplot(EN,outline=F, horizontal=T)
 boxplot(EN,outline=F, horizontal=T, axes=T) 
