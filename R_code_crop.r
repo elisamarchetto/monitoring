5657e03e0dd2d29357ecae31d2994a75e576f28d
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2020-06-09 16:15:31 +0200

    Update R_code_crop.r

--- R_code_crop.r
+++ R_code_crop.r
@@ -3,7 +3,7 @@ library(ncdf4)
 library(raster)
 
 snow <- raster("c_gls_SCE_202005260000_NHEMI_VIIRS_V1.0.1.nc")
-ext <- c(0, 20, 35, 50)
+ext <- c(0, 20, 35, 50) # set the extention desired
 zoom(snow, ext=ext)
 snowitaly <- crop(snow, ext)
-zoom(snow, ext=drawExtent())
+zoom(snow, ext=drawExtent()) # draw directly the extent from the graph
