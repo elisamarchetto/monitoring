c56629fc583334abc058ba40e6945215d13bdd53
Elisa Marchetto <62661809+elisamarchetto@users.noreply.github.com>
2020-06-10 18:18:55 +0200

    Update R_code_EBVs.r

--- R_code_EBVs.r
+++ R_code_EBVs.r
@@ -9,11 +9,11 @@ snt <- brick("snt_r10.tif")
 plot(snt)
 plotRGB(snt,3,2,1, stretch="lin")
 plotRGB(snt,4,2,1, stretch="lin")
-# how the different layers(bands) are related... for a better understanding of PCA analysis
-pairs(snt)
+
+pairs(snt)# how the different layers(bands) are related... for a better understanding of PCA analysis
 sntpca <- rasterPCA(snt)
 #information about the output of the model, in other words the percentage of variance related to the components
-summary(sntpca$model) # proportion of variance: 0.7015076 for the PC1(good approximation)
+summary(sntpca$model) # proportion of variance: 0.7015076 for the PC1(good approximation), almost the 70%
 plotRGB(sntpca$map, 1, 2, 3, stretch="lin")
 #calculte the standard deviation using a moving window (5x5)
 # create the moving window: it is a matrix
@@ -23,11 +23,12 @@ sd_snt <- focal(sntpca$map$PC1, w=window, fun=sd)
 cl <- colorRampPalette(c('dark blue','green','orange','red'))(100) 
 plot(sd_snt, col=cl)
 
-##folcal function can be applied also to a image directly taken in field: cladonia.jpg
+##folcal function can be applied also to an image directly taken in field: cladonia.jpg
+
 #library(raster), to open the image with several layers: brick function
 clad <- brick("cladonia_stellaris_calaita.JPG")
 plotRGB(clad, 1,2,3, stretch="Lin")
-window <- matrix(1, nrow = 3, ncol = 3) # 3x3 pixel is the extent of moving window, 1 is a number that not impact the calculation
+window <- matrix(1, nrow = 3, ncol = 3) # 3x3 pixel is the extent of moving window (moving through the matrix by 3x3 pixels, obtainig 1 pixel). 1 is a number that not impact the calculation
 # recall library RStoolbox for performing PCA analysis. The goal is to apply standard deviation to the principal component
 cladpca <- rasterPCA(clad) #the bands are correlated each other hence the PC describe 98%
 sd_clad <- focal(cladpca$map$PC1, w=window, fun=sd) # show the structural complexity of cladonia, variability of a single organism
